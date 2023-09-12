f22555519b8c2a929e4a61335903b85327eee7af
sundar2568223 <139097521+sundar2568223@users.noreply.github.com>
2023-09-12 22:32:10 +0530

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,33 @@
+﻿namespace day1ass1
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string Login()
+        {
+            if (string.IsNullOrWhiteSpace(Username) && string.IsNullOrWhiteSpace(Password))
+            {
+                return "Provide Username and Password";
+            }
+            else if (string.IsNullOrWhiteSpace(Username))
+            {
+                return "Provide Username";
+            }
+            else if (string.IsNullOrWhiteSpace(Password))
+            {
+                return "Provide Password";
+            }
+            else if (Username == "Sravan" && Password == "s@123")
+            {
+                return "Login Successful";
+            }
+            else
+            {
+                return "Login Failed";
+            }
+        }
+
+    }
+}
